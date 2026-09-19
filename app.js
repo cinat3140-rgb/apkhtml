2b0c8fca6798ab0a4b93334bad913dc8aee697f2
cinat3140-rgb <cinat3140-rgb@users.noreply.github.com>
2026-09-19 13:52:51 +0300

    sohbet kutusu oyun detayinin en altina tasindi + detail-social stili

--- app.js
+++ app.js
@@ -292,7 +292,6 @@
       : "";
 
     el.innerHTML =
-      '<div class="detail-social" id="detailSocial" data-gid="' + (g.id) + '"></div>' +
       '<div class="detail-head">' +
           '<div class="detail-cover-wrap">' + coverWithFallback(g) + (g.isFeatured ? '<span class="gcard-featured">★ Öne Çıkan</span>' : "") + "</div>" +
           '<div class="detail-titleblock">' +
@@ -314,7 +313,8 @@
           '<div class="detail-panel-title">APK Bilgileri</div>' +
           '<div class="info-list">' + infoRows + "</div>" +
         "</aside>" +
-      "</div>";
+      "</div>" +
+      '<div class="detail-social" id="detailSocial" data-gid="' + (g.id) + '"></div>';
     setTimeout(function () { initComments(g.id); }, 0);
   }
 
